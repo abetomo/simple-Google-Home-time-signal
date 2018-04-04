b04aa75fbad574706b3102bda76b0801b5cc429b
abetomo <abe@enzou.tokyo>
2018-04-04 16:16:59 +0900

    Add Japanese time signal script

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const googlehome = require('google-home-notifier');
+
+const deviceName = 'Google Home'
+const ip = process.argv[2] || '192.168.1.20'
+
+const hour = (new Date()).getHours()
+const text = hour + '時です'
+console.log(text)
+
+const language = 'ja'
+
+googlehome.ip(ip, language)
+googlehome.device(deviceName,language)
+googlehome.notify(text, (notifyRes) => {
+  console.log(notifyRes)
+})
