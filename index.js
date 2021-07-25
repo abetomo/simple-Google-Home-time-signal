9cc8a8a0583b0b33c3ea59e502000f3d1b69010a
abetomo <abe@enzou.tokyo>
2021-07-25 09:29:15 +0900

    feat: switch to `google-home-push`

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 'use strict'
 
-const googlehome = require('google-home-notifier')
+const GoogleHome = require('google-home-push')
 
-const deviceName = 'Google Home'
 const ip = process.argv[2] || '192.168.1.20'
+const language = 'ja'
+
+const options = { language }
+const myHome = new GoogleHome(ip, options)
 
 const hour = (new Date()).getHours()
 const text = hour + '時です'
 console.log(text)
 
-const language = 'ja'
-
-googlehome.ip(ip, language)
-googlehome.device(deviceName, language)
-googlehome.notify(text, (notifyRes) => {
-  console.log(notifyRes)
-})
+myHome.speak(text)
